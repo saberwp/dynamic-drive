d437b9b7ee99b4ed45d69c5b21833367226f82d3
Casey Milne <casey@saberwp.com>
2022-04-02 23:54:23 -0600

    Init commit.

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// There is nothing output here because block themes do not use php templates.
+// There is a core ticket discussing removing this requirement for block themes:
+// https://core.trac.wordpress.org/ticket/54272.
+
+<?php get_header(); ?>
+
+
+
+<?php get_footer(); ?>
